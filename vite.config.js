dce546ee1980db292482e5b0ec0e8f4f58861146
longevid <longevid@example.com>
2026-02-28 23:00:02 -0300

    init2

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(new URL('./src', import.meta.url).pathname),
     },
     extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
   },
